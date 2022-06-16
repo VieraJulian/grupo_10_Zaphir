8194f8fd6be817eea83f485db5a6c912bee3786f
VieraJulian <julianviera2019@hotmail.com>
2022-06-16 09:55:47 -0300

    Julián - Empezando, carpetas y servidor leantado

---
+++ source/app.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+
+const public = path.resolve(__dirname, '../public');
+
+app.use(express.static(public));
+
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "/views/home.html")));
