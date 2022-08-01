72bc6d464e7abdbc6a87b66614f77f4292ffd516
VieraJulian <julianviera2019@hotmail.com>
2022-08-01 17:47:06 -0300

    Julián - errores de validaciones

--- source/validations/register.validations.js
+++ source/validations/register.validations.js
@@ -2,7 +2,7 @@ const { body } = require('express-validator')
 const { index } = require('../models/users.model')
 
 const register = [
-    body('nombre').notEmpty().withMessage('Este campo no puede quedar vacío').bail().isLength({min: 2}).withMessage("el nombre debe contener mínimo dos caracteres").bail(),
+    body("nombre").notEmpty().withMessage("El nombre no puede quedar vacío").bail().isLength({min : 2}).withMessage("El nombre debe contener mínimo dos caracteres").bail(),
     body('email').notEmpty().withMessage('El email no puede quedar vacío').bail().isEmail().withMessage('El formato de email no es válido').bail().custom(value => {
         let users = index()
         users = users.map(u => u.email)
