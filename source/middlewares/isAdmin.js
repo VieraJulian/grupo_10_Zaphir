a9eb7c57c9743f8c4e05cf254bdbf7849a4444cd
VieraJulian <julianviera2019@hotmail.com>
2022-08-06 09:29:17 -0300

    Julián - boton de logout funciona, falta darle estilo

---
+++ source/middlewares/isAdmin.js
@@ -0,0 +1,10 @@
+const isAdmin = (req, res, next) => {
+
+    if(req.session.user && req.session.user.isAdmin) {
+        next();
+    }
+
+    return res.redirect("/");
+}
+
+module = isAdmin
--- source/middlewares/user.js
+++ source/middlewares/user.js
@@ -1,8 +1,11 @@
 const middleware = (req, res, next) => {
+
     let user = null;
+
     if (req.session && req.session.user) {
         user = req.session.user
     }
+
     res.locals.user = user
 
     return next()
