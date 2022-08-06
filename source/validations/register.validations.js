15b11551fb081bc844200add98d7dbc5fbcf8a6d
VieraJulian <julianviera2019@hotmail.com>
2022-08-06 17:17:23 -0300

    Julián - arreglos en las validaciones

--- source/validations/register.validations.js
+++ source/validations/register.validations.js
@@ -3,7 +3,7 @@ const { index } = require('../models/users.model')
 
 const register = [
     body("nombre").notEmpty().withMessage("El nombre no puede quedar vacío").bail().isLength({min : 2}).withMessage("El nombre debe contener mínimo dos caracteres").bail(),
-    body("email").notEmpty().withMessage('El email no puede quedar vacío').bail().isEmail().withMessage('El formato de email no es válido').bail().custom(value => {
+    body("email").notEmpty().withMessage("El email no puede quedar vacío").bail().isEmail().withMessage('El formato de email no es válido').bail().custom(value => {
         let users = index()
         users = users.map(u => u.email)
         if(users.includes(value)){
