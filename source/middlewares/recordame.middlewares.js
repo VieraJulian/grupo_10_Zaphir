932beba22873602d804c666714e9ce75931b747e
VieraJulián <julianviera2019@hotmail.com>
2022-08-09 22:30:23 -0300

    Julián arreglos en las cookies

--- source/middlewares/recordame.middlewares.js
+++ source/middlewares/recordame.middlewares.js
@@ -5,7 +5,7 @@ const recordame = (req, res, next) => {
     if (req.cookies.recordame != undefined && req.session.user == undefined) {
         let users = index()
         let user = users.find(u => u.email === req.cookies.recordame)
-        /* delete user.password */
+        delete user.password
         req.session.user = user
     }
 
