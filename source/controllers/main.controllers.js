9fb1e62d2b6295015befadb20f6d1a6c65162417
VieraJulian <julianviera2019@hotmail.com>
2022-08-28 17:32:20 -0300

    Julián - Vista home con sequelize

--- source/controllers/main.controllers.js
+++ source/controllers/main.controllers.js
@@ -1,11 +1,20 @@
-const { index } = require("../models/products.models");
-const { product } = require("../database/models/index")
+const { product } = require("../database/models/index");
+const { Op } = require("sequelize");
 
 module.exports = {
     home: async (req, res) => {
 
-        let products = await product.findAll()
-
+        let products = await product.findAll({
+            include: [
+                { association: "images" }
+            ],
+            where: {
+                descuento: {
+                    [Op.gt]: 0
+                }
+            },
+            limit: 4
+        })
         return res.render("home", {
             title: "Zaphir",
             styles: ["home-mobile", "home-tablets", "home-desktop"],
