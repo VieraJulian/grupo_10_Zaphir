f47d45bf6da571f5859064e8080bfc97a801bf09
VieraJulian <julianviera2019@hotmail.com>
2022-09-30 20:43:46 -0300

    Julián - css products dashboard terminada

--- dashboard/src/pages/Dashboard.jsx
+++ dashboard/src/pages/Dashboard.jsx
@@ -8,26 +8,29 @@ let users = await allUsers();
 
 function Dashboard() {
     return (
-        <div className="home-cont">
-            <div className="div-cont">
-                <p className='top-p'>Total de productos:</p>
-                <p className="bottom-p"> {data.count}</p>
+        <div className='main'>
+            <div className="home-cont">
+                <div className="div-cont">
+                    <p className='top-p'>Total de productos:</p>
+                    <p className="bottom-p"> {data.count}</p>
+                </div>
+                <div className="div-cont">
+                    <p className='top-p'>Total de categorias:</p>
+                    <p className="bottom-p">{data.categories.length}</p>
+                </div>
+                <div className="div-cont">
+                    <p className='top-p'>Productos por categorias:</p>
+                    <ul className='prod-ul'>
+                        {data.categories.map((c, i) => <li className="prod-li" key={i}>{c}: <span className='span-cat'>{data.countByCategory[c]}</span></li>)}
+                    </ul>
+                </div>
+                <div className="div-cont">
+                    <p className='top-p'>Total de usuarios:</p>
+                    <p className="bottom-p">{users.count}</p>
+                </div>
             </div>
-            <div className="div-cont">
-                <p className='top-p'>Total de categorias:</p>
-                <p className="bottom-p">{data.categories.length}</p>
-            </div>
-            <div className="div-cont">
-                <p className='top-p'>Productos por categorias:</p>
-                <ul className='prod-ul'>
-                    {data.categories.map((c, i) => <li className="prod-li"key={i}>{c}: <span className='span-cat'>{data.countByCategory[c]}</span></li>)}
-                </ul>
-            </div>
-            <div className="div-cont">
-                <p className='top-p'>Total de usuarios:</p>
-                <p className="bottom-p">{users.count}</p>
-            </div>
-        </div>)
+        </div>
+    )
 }
 
 export default Dashboard;
--- dashboard/src/pages/Products.jsx
+++ dashboard/src/pages/Products.jsx
@@ -1,36 +1,42 @@
 import { Link } from "react-router-dom"
 import { getAll } from '../services/products';
+import '../../public/css/Products-mobile.css'
+import '../../public/css/Products-tablet.css'
+import '../../public/css/Products-desktop.css'
 let data = await getAll();
 const lastProduct = data.products[data.products.length - 1]
 
 function Products() {
     return (
-        <>
-            <div>
-                <p>Productos:</p>
-                <ul>
-                    {data.products.map((product, index) => <li key={index}><Link to={`/product/${product.id}`}>{product.nombre}</Link></li>)}
-                </ul>
+        <div className='main'>
+
+            <div className="prod-cont">
+                <div className="div-cont-prod">
+                    <p className="top-p-prod">Productos:</p>
+                    <ul>
+                        {data.products.map((product, index) => <li className="prod-list" key={index}><Link to={`/product/${product.id}`}>{product.nombre}</Link></li>)}
+                    </ul>
+                </div>
+                <div className="div-cont-prod">
+                    <p className="top-p-prod">Último producto creado:</p>
+                    <p className="last-prod">Id: <span className="span-last">{lastProduct.id}</span></p>
+                    <p className="last-prod">Categoria: <span className="span-last">{lastProduct.categoria}</span></p>
+                    <p className="last-prod">Nombre: <span className="span-last">{lastProduct.nombre}</span></p>
+                    <p className="last-prod">Descripción: <span className="span-last">{lastProduct.descripcion}</span></p>
+                    <p className="last-ct">Colores:</p>
+                    <ul className="ul-prod">
+                        {lastProduct.colores.map((c, i) => <li className="li-ct" key={i}><span className="span-last">{c}</span></li>)}
+                    </ul>
+                    <p className="last-ct">Talles:</p>
+                    <ul className="ul-prod">
+                        {lastProduct.talles.map((t, i) => <li className="li-ct" key={i}><span className="span-last">{t}</span></li>)}
+                    </ul>
+                    <picture className="cont-img-prod">
+                        <img className="img-prod" src={lastProduct.imagenes[0]} alt={lastProduct.nombre} />
+                    </picture>
+                </div>
             </div>
-            <div>
-                <p>Último producto creado:</p>
-                <p>Id: {lastProduct.id}</p>
-                <p>Categoria: {lastProduct.categoria}</p>
-                <p>Nombre: {lastProduct.nombre}</p>
-                <p>Descripción: {lastProduct.descripcion}</p>
-                <p>Colores:</p>
-                <ul>
-                    {lastProduct.colores.map((c, i) => <li key={i}>{c}</li>)}
-                </ul>
-                <p>Talles:</p>
-                <ul>
-                    {lastProduct.talles.map((t, i) => <li key={i}>{t}</li>)}
-                </ul>
-                <picture>
-                    <img src={lastProduct.imagenes[0]} alt={lastProduct.nombre} />
-                </picture>
-            </div> 
-        </>
+        </div>
     )
 }
 
