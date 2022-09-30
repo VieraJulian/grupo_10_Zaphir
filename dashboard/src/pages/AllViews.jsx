072a128c92fcff711f3af08345a7540f9f757d52
VieraJulian <julianviera2019@hotmail.com>
2022-09-30 18:13:30 -0300

    Julián - css interfaz del dashboard

--- dashboard/src/pages/AllViews.jsx
+++ dashboard/src/pages/AllViews.jsx
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom'
+import '../../public/css/AllViews-mobile.css'
+import '../../public/css/AllViews-tablet.css'
+import '../../public/css/AllViews-desktop.css'
 
 function AllViews() {
     return (
-        <>
-            <picture>
-                <img src="../../public/img/LogoZP.png" alt="" />
+        <div className="contenedor">
+            <picture className="cont-img">
+                <img className="logo" src="../../public/img/LogoZP.png" alt="Logo Zaphir" />
             </picture>
-            <nav>
-                <Link to="/"><i class="fa-solid fa-house"></i></Link>
-                <Link to="/products"><i class="fa-solid fa-shirt"></i></Link>
-                <Link to="/users"><i class="fa-solid fa-users"></i></Link>
+            <nav className="nav-icon">
+                <Link to="/"><i class="fa-solid fa-house icon"></i></Link>
+                <Link to="/products"><i class="fa-solid fa-shirt icon"></i></Link>
+                <Link to="/users"><i class="fa-solid fa-users icon"></i></Link>
             </nav>
-        </>
+        </div>
     )
 }
 
