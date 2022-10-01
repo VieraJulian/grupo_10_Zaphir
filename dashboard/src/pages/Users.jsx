8fe7480e4da125dd20273f28fe938ca903d3cc4c
VieraJulian <julianviera2019@hotmail.com>
2022-10-01 09:00:38 -0300

    Julián - css users dashboard terminado

--- dashboard/src/pages/Users.jsx
+++ dashboard/src/pages/Users.jsx
@@ -1,25 +1,29 @@
 import { Link } from 'react-router-dom'
 import { allUsers } from '../services/users'
+import '../../public/css/Users-mobile.css'
+import '../../public/css/Users-desktop.css'
 let users = await allUsers();
-const lastUser = users.users[users.users.length - 1] 
+const lastUser = users.users[users.users.length - 1]
 
 function Users() {
     console.log(users)
     return (
-        <>
-            <div>
-                <p>Usuarios:</p>
-                <ul>
-                    {users.users.map((u, i) => <li key={i}><Link to={`/user/${u.id}`}>{u.nombre}</Link></li>)}
-                </ul>
+        <div className='main'>
+            <div className='cont-users'>
+                <div className='div-cont-user'>
+                    <p className='top-p-user'>Usuarios:</p>
+                    <ul>
+                        {users.users.map((u, i) => <li className="li-users" key={i}><Link to={`/user/${u.id}`}>{u.nombre}</Link></li>)}
+                    </ul>
+                </div>
+                <div className='div-cont-user'>
+                    <p className='top-p-user'>Último usuario:</p>
+                    <p className='last-p-user'>Id: <span className='span-user'>{lastUser.id}</span></p>
+                    <p className='last-p-user'>Nombre: <span className='span-user'>{lastUser.nombre}</span></p>
+                    <p className='last-p-user'>Email: <span className='span-user'>{lastUser.email}</span></p>
+                </div>
             </div>
-            <div>
-                <p>Último usuario:</p>
-                <p>Id: {lastUser.id}</p>
-                <p>Nombre: {lastUser.nombre}</p>
-                <p>Email: {lastUser.email}</p>
-            </div>
-        </>
+        </div>
     )
 }
 
