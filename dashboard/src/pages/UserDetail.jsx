015654a9f2fc01700a39fa4470106df3d2a1cde1
VieraJulian <julianviera2019@hotmail.com>
2022-10-01 10:46:25 -0300

    Julián - css user detail terminado

--- dashboard/src/pages/UserDetail.jsx
+++ dashboard/src/pages/UserDetail.jsx
@@ -1,5 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from 'react';
+import '../../public/css/UserDetail-mobile.css'
+import '../../public/css/UserDetail-desktop.css'
 import { oneUser } from '../services/users'
 
 function UserDetail() {
@@ -10,16 +12,22 @@ function UserDetail() {
     }, [id])
 
     return (
-        <>
-            <p>{user.nombre}</p>
-            <p>Id: {user.id}</p>
-            <p>Email: {user.email}</p>
-            <p>Teléfono: {user.telefono ? user.telefono : "Sin teléfono"}</p>
-            <p>Avatar:</p>
-            <picture>
-                <img src={user.imagen} alt={`avatar-${user.nombre}`} />
-            </picture>
-        </>
+        <div className="main-user">
+            <div className="cont-user">
+                <div className="div-user-left">
+                    <p className="p-user-name">{user.nombre}</p>
+                    <p className="p-user">Id:</p><div>{user.id}</div>
+                    <p className="p-user">Email:</p><div>{user.email}</div>
+                    <p className="p-user">Teléfono:</p><div>{user.telefono ? user.telefono : "Sin teléfono"}</div>
+                </div>
+                <div className="div-user-right">
+                    <p className="p-user">Avatar:</p>
+                    <picture>
+                        <img className="avatar-user" src={user.imagen} alt={`avatar-${user.nombre}`} />
+                    </picture>
+                </div>
+            </div>
+        </div>
     )
 }
 
